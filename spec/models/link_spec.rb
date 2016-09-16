a3597ddf47457f1e87f5db6565976269e353a2a9
Jack Schuss <jackschuss@gmail.com>
2016-09-16 12:54:06 +0300

    add shorten test

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+  describe "#shorten" do
+
+    it "creates a base 36 combo and returns a string" do
+      link = Link.create(url: "http://www.google.com")
+
+      link.shorten
+
+      expect(link.short).to be_a(String)
+    end
+  end
+
 end
