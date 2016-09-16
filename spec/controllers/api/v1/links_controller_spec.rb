3c3fe2b10a17237ab4e8e3f1edacab5201b2a5ec
Jack Schuss <jackschuss@gmail.com>
2016-09-16 13:24:57 +0300

    add happy path controller test

--- spec/controllers/api/v1/links_controller_spec.rb
+++ spec/controllers/api/v1/links_controller_spec.rb
@@ -2,4 +2,26 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::LinksController, type: :controller do
 
+  describe "#create" do
+    context "valid link data" do
+      subject { post :create, format: :json,  url: "http://www.google.com" }
+
+      it "returns http status success" do
+        subject
+        expect(response).to have_http_status(:success)
+      end
+
+      it "it increases the Link count by 1" do
+        expect{subject}.to change(Link, :count).by(1)
+      end
+
+      it "returns a json response" do
+        subject
+        expect(JSON.parse(response.body)).to eq({"success" => true, "url" =>"http://www.google.com", "short" => Link.last.full_short_url})
+      end
+    end
+
+
+  end
 end
+
