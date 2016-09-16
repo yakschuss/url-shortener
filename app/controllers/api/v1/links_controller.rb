fee7eebe048faae2a3563ec01aafcad1e1dfe8eb
Jack Schuss <jackschuss@gmail.com>
2016-09-16 13:28:12 +0300

    pass happy path test

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::LinksController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def create
+    link = Link.create(url: params[:url])
+
+    if link.save
+      render json: {success: true, url: link.url, short: link.full_short_url }.as_json
+    else
+      render json: {message: "not yet implemented"}
+    end
+  end
+
+
 end
